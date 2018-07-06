b1f33f7a0c13b23a67afbabf8260fff6f0495067
Farah Abdi <abdi.f35@gmail.com>
2018-07-06 18:18:16 +1000

    Minimal

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import * as React from 'react';
+import { Button } from './common';
+
+class App extends React.Component {
+
+    render() {
+        return (
+        <div>
+            <div>ff</div>
+            <Button />
+        </div>
+        );
+    }
+}
+
+export default App;
